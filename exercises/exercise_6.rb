e73fca023778564bf1fdda2671efcf1947f9fe8a
Don-Stevenson <dsteven1@gmail.com>
2020-02-03 22:16:08 -0500

    working 6 and 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Jean", last_name: "C", hourly_rate: 6)
+@store1.employees.create(first_name: "Phil", last_name: "M", hourly_rate: 15)
+@store1.employees.create(first_name: "Jake", last_name: "T. Snake", hourly_rate: 32)
+
+@store2.employees.create(first_name: "Jim", last_name: "Duggan", hourly_rate: 15)
+@store2.employees.create(first_name: "George", last_name: "Steele", hourly_rate: 10)
+@store2.employees.create(first_name: "Jesse", last_name: "Ventura", hourly_rate: 60)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+# Your code goes here ...
+puts "Add your store"
+store_name = gets.chomp
+
+store7 = Store.create({
+:name => store_name,
+:annual_revenue => -100000,
+:mens_apparel => true,
+:womens_apparel => false
+})
+
+puts store7.errors.full_messages
